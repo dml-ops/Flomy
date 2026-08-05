0e5cc259cc06187c301c8a1615c30055b5b2c072
dml <dml.ops@outlook.com>
2026-08-05 16:41:44 +0100

    Fix variable references in network security rule

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -42,6 +42,6 @@ resource "azurerm_network_security_rule" "ssh" {
   destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.oyster-rg.name
+  resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.oyster-nsg.name
 }
