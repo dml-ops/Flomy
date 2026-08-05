154e989d474807c7fd268668c29a782a07b828c7
dml <dml.ops@outlook.com>
2026-08-05 17:24:40 +0100

    Add inbound network security rule for HTTP port 80

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -32,6 +32,20 @@ resource "azurerm_network_security_group" "oyster-nsg" {
   resource_group_name = var.resource_group_name
 }
 
+resource "azurerm_network_security_rule" "http" {
+  name                        = "allow-http"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.oyster-nsg.name
+}
+
 resource "azurerm_network_security_rule" "ssh" {
   name                        = "allow-ssh"
   priority                    = 100
