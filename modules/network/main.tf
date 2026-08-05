5673c2304d0873506a0aa0e6ab422837d95fcc1c
dml <dml.ops@outlook.com>
2026-08-05 16:37:40 +0100

    Add inbound network security rule for SSH port 22

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -31,3 +31,17 @@ resource "azurerm_network_security_group" "oyster-nsg" {
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.oyster-rg.name
+  network_security_group_name = azurerm_network_security_group.oyster-nsg.name
+}
